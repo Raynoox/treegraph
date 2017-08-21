d1223e7e013258538e06bcc0423761147f8fe09b
DamianG <damian.grzeszczuk@gmail.com>
2017-08-21 18:55:44 +0200

    initial commit

---
+++ src/test/java/dg/project/treegraph/TreegraphApplicationTests.java
@@ -0,0 +1,16 @@
+package dg.project.treegraph;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TreegraphApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
